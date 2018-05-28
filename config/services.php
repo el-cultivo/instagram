9fef9760f2720ba2442dffda8e90a35ae6ef5938
Sergio Fonseca <s@elcultivo.mx>
2018-05-28 15:39:54 -0500

    Se pasan env a config de instagram

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'instagram' => [
+        'client_id' => env('INSTAGRAM_CLIENT_ID'),
+        'client_secret' => env('INSTAGRAM_CLIENT_SECRET'),
+        'redirect' => url('/') . '/callback',
+    ],
+
 ];
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Route::get('/', function (){
 Route::get('/redirect', function (){
 
     $query = http_build_query([
-        'client_id' => env('INSTAGRAM_CLIENT_ID'),
-        'redirect_uri' => url('/') . '/callback',
+        'client_id' => config('services.instagram.client_id'),
+        'redirect_uri' => config('services.instagram.redirect'),
         'response_type' => 'code',
         'scope' => 'public_content',
     ]);
@@ -40,9 +40,9 @@ Route::get('/callback', function (Illuminate\Http\Request $request) {
         $response = $http->post('https://api.instagram.com/oauth/access_token/', [
             'form_params' => [
                 'grant_type' => 'authorization_code',
-                'client_id' => env('INSTAGRAM_CLIENT_ID'),
-                'client_secret' => env('INSTAGRAM_SECRET'),
-                'redirect_uri' => url('/') . '/callback',
+                'client_id' => config('services.instagram.client_id'),
+                'client_secret' => config('services.instagram.client_secret'),
+                'redirect_uri' => config('services.instagram.redirect'),
                 'code' => $request->code,
             ],
         ]);
